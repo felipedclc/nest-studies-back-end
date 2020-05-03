e72331707279588c89bbf1be27dca8f0775b1d67
Diego Fernandes <diegofsilva.fernandes@gmail.com>
2020-05-03 00:01:26 -0300

    Initial import

---
+++ src/jogadores/jogadores.service.ts
@@ -0,0 +1,65 @@
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto'
+import { Jogador } from './interfaces/jogador.interface'
+import * as uuid from 'uuid/v1'
+
+@Injectable()
+export class JogadoresService {
+
+    private jogadores: Jogador[] = [];
+
+    private readonly logger = new Logger(JogadoresService.name)
+
+    async criarAtualizarJogador(criaJogadorDto: CriarJogadorDto): Promise<void> {
+
+        const { email } = criaJogadorDto
+
+        const jogadorEncontrado = await this.jogadores.find(jogador => jogador.email === email)
+
+        if (jogadorEncontrado) {
+            await this.atualizar(jogadorEncontrado, criaJogadorDto)
+        } else {
+            await this.criar(criaJogadorDto)
+        }
+    
+
+    }
+
+    async consultarTodosJogadores(): Promise<Jogador[]> {
+        return await this.jogadores;
+    }
+
+    async consultarJogadorPeloEmail(email: string): Promise<Jogador> {
+        const jogadorEncontrado = await this.jogadores.find(jogador => jogador.email === email)
+        if (!jogadorEncontrado) {
+            throw new NotFoundException(`Jogador com e-mail ${email} não encontrado`)
+        }
+        return jogadorEncontrado
+    }
+
+    async deletarJogador(email): Promise<void> {
+        const jogadorEncontrado = await this.jogadores.find(jogador => jogador.email === email)
+        this.jogadores = this.jogadores.filter(jogador => jogador.email !== jogadorEncontrado.email)
+    }
+ 
+    private criar(criaJogadorDto: CriarJogadorDto): void {
+        const { nome, telefoneCelular, email } = criaJogadorDto
+        const jogador: Jogador = {
+            _id: uuid(),
+            nome,
+            telefoneCelular,
+            email,
+            ranking: 'A',
+            posicaoRanking: 1,
+            urlFotoJogador: 'www.google.com.br/foto123.jpg'
+        };
+        this.logger.log(`criaJogadorDto: ${JSON.stringify(jogador)}`)
+        this.jogadores.push(jogador);
+    }
+
+    private atualizar(jogadorEncontrado: Jogador, criarJogadorDto: CriarJogadorDto): void {
+        const { nome } = criarJogadorDto
+        jogadorEncontrado.nome = nome;
+    }
+
+}
